c28f8269938a6b9235f9b8d25bdac45139f34a4e
tmmoond8 <tmmoond8@gmail.com>
2022-01-16 12:30:29 +0900

    🍎 애플 로그인 컴포넌트 작성

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -1,7 +1,13 @@
 import auth from '@react-native-firebase/auth';
 
-export async function SignIn(idToken) {
+export async function signInByGoogle(idToken) {
   const credential = await auth.GoogleAuthProvider.credential(idToken);
+  const userCredential = auth().signInWithCredential(credential);
+  return userCredential;
+}
+
+export async function signInByApple(idToken, nonce) {
+  const credential = await auth.AppleAuthProvider.credential(idToken, nonce);
   const userCredential = auth().signInWithCredential(credential);
   return userCredential;
 }
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -6,7 +6,11 @@ import {
   GoogleSigninButton,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
-import {SignIn} from '../libs/auth';
+import {
+  AppleButton,
+  appleAuth,
+} from '@invertase/react-native-apple-authentication';
+import {signInByGoogle, signInByApple} from '../libs/auth';
 import {useUserContext} from '../contexts/userContext';
 import {getUser, createUser} from '../libs/users';
 
@@ -18,7 +22,7 @@ export default function SignInScreen() {
       setIsProcessing(true);
       await GoogleSignin.hasPlayServices();
       const {idToken} = await GoogleSignin.signIn();
-      const {user} = await SignIn(idToken);
+      const {user} = await signInByGoogle(idToken);
       const currentUser = await getUser(user.uid);
       if (!currentUser) {
         await createUser({
@@ -36,16 +40,48 @@ export default function SignInScreen() {
     } catch (error) {
       console.log('error', error);
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        setIsProcessing(false);
         // user cancelled the login flow
       } else if (error.code === statusCodes.IN_PROGRESS) {
         // operation (f.e. sign in) is in progress already
       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        setIsProcessing(false);
         // play services not available or outdated
       } else {
-        // some other error happened
+        setIsProcessing(false);
+        // some other error happezned
       }
     }
   };
+
+  const handleAppleSignIn = async () => {
+    setIsProcessing(true);
+    try {
+      const {identityToken, nonce} = await appleAuth.performRequest({
+        requestedOperation: appleAuth.Operation.LOGIN,
+        requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+      });
+      const {user} = await signInByApple(identityToken, nonce);
+      const currentUser = await getUser(user.uid);
+      if (!currentUser) {
+        await createUser({
+          id: user.uid,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+        });
+      }
+      setUser(
+        currentUser ?? {
+          ...user,
+          id: user.uid,
+        },
+      );
+    } catch (error) {
+      console.error(error);
+    }
+    setIsProcessing(false);
+  };
+
   React.useEffect(() => {
     StatusBar.setBarStyle('dark-content');
     GoogleSignin.configure({
@@ -57,13 +93,19 @@ export default function SignInScreen() {
 
   return (
     <View style={styles.fullScreen}>
-      <Text style={styles.title}>HighBall Sign In</Text>
+      <Text style={styles.title}>HighBall SignIn</Text>
       <GoogleSigninButton
-        style={{width: 192, height: 48}}
+        style={styles.googleButton}
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Dark}
         onPress={handleGoogleSignIn}
         disabled={isProcessing}
+      />
+      <AppleButton
+        buttonStyle={AppleButton.Style.BLACK}
+        buttonType={AppleButton.Type.SIGN_IN}
+        style={styles.appbleButton}
+        onPress={handleAppleSignIn}
       />
     </View>
   );
@@ -80,7 +122,13 @@ const styles = StyleSheet.create({
     fontSize: 32,
     marginBottom: 50,
   },
-  button: {
-    backgroundColor: 'red',
+  googleButton: {
+    width: 192,
+    height: 48,
+  },
+  appbleButton: {
+    width: 186,
+    height: 44,
+    marginTop: 8,
   },
 });
